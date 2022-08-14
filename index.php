5339c0239bd62087c6311f21abdea17b877b2b38
GofN <97667889+dnamgithub33@users.noreply.github.com>
2022-08-14 13:08:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
-$hostname = 'localhost:3306';
-$username = 'root';
-$password = '';
-$dbname = "dangnam1";
-$conn = mysqli_connect($hostname, $username, $password,$dbname);
-
+	include 'connect.php';
 ?>
 <html lang="en-US">
 <head>
