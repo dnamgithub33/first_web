438b09d3d48f07fc62b32588c7bfc630955ca3ed
GofN <97667889+dnamgithub33@users.noreply.github.com>
2022-08-14 13:08:35 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -4,5 +4,6 @@ $username = 'root';
 $password = '';
 $dbname = "dangnam1";
 $conn = mysqli_connect($hostname, $username, $password,$dbname);
-
+/*if($conn)      kiểm tra kết nối db
+    echo "<script>alert('ok')</script>";*/
 ?>
