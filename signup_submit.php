34f0c1dea0e782208904c7490e4a11e2fc112723
GofN <97667889+dnamgithub33@users.noreply.github.com>
2022-08-14 13:10:08 +0700

    Update signup_submit.php

--- signup_submit.php
+++ signup_submit.php
@@ -1,25 +1,28 @@
 <?php
     include 'connect.php';
-    if(isset($_POST['submit']) && $_POST['username'] != '' && $_POST['repassword'] !=''&& $_POST['password'] !=''){
+    if(isset($_POST["submit"])){
         $username= $_POST['username'];
         $password= $_POST['password'];
         $repassword= $_POST['repassword'];
+        $level =0;
         if($password !=$repassword){
-            
-            header('location:signup.php');
+            echo '<script>alert("fail phần rep và p")</script>';
+            //header('location:signup.php');
         }
         $sql= "SELECT * FROM user WHERE username='$username";
         $got= mysqli_query($conn,$sql);
         $password=md5($password);
         if(mysqli_num_rows($got)>0){
-            header('location:signup.php');
-        }else{
             $sql = "INSERT INTO user (username,password,level) VALUES ('$username','$password','$level')";
             mysqli_query($conn,$sql);
             echo '<script>alert("Congratulation")</script>';
+        }else{
+            echo '<script>alert("fail")</script>';
+            //header('location:signup.php');
         }
         
     } else{
-        header('location:signup.php');
+        echo '<script>alert("fail điều kiện đầu")</script>';
+       // header('location:signup.php');
     }
 ?>
