76ca60eb956907a566eb4c383c5f93c25a98a12b
GofN <97667889+dnamgithub33@users.noreply.github.com>
2022-08-17 23:40:22 +0700

    Delete connect.php

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$hostname = 'localhost:3306';
-$username = 'root';
-$password = '';
-$dbname = "dangnam1";
-$conn = mysqli_connect($hostname, $username, $password,$dbname);
-/*if($conn)      kiểm tra kết nối db
-    echo "<script>alert('ok')</script>";*/
-?>
